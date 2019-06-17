a16101a4d1a0182ea3bad2589f4cadc2a5847d18
Eliot_Cheng <eliot46148@gmail.com>
2019-06-17 10:47:01 +0800

    基本Action_Mode

--- Lottery/app/src/main/java/com/example/lottery_2/MainActivity.java
+++ Lottery/app/src/main/java/com/example/lottery_2/MainActivity.java
@@ -7,13 +7,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -28,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
     private ListAdapter mlistadapter;
     private ArrayList<ItemSet> itemSets;
     private Bundle bundle;
+    private FirebaseAuth mAuth;
+    private AbsListView.MultiChoiceModeListener mMultiChoiceModeListener;
+    private View mMultiSelectionbarView;
+    private TextView mSelectedItemCount;
+    private boolean allCheckedMode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,10 +49,62 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         bundle = new Bundle();
         itemSets = new ArrayList<>();
+        mMultiChoiceModeListener = new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+                Log.d("TAG", "onItemCheckedStateChanged");
+                mode.invalidate();
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                Log.d("TAG", "onCreateActionMode");
+                //allCheckedMode = false;
+                getMenuInflater().inflate(R.menu.menu_multi_choice, menu);
+                if (mMultiSelectionbarView == null) {
+                    mMultiSelectionbarView = LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_view, null, false);
+                }
+                mode.setCustomView(mMultiSelectionbarView);
+                ((TextView) mMultiSelectionbarView.findViewById(R.id.title)).setText("選擇項目");
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                Log.d("TAG", "onPrepareActionMode");
+                return true;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                Log.d("TAG", "onActionItemClicked");
+                return false;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                Log.d("TAG", "onDestroyActionMode");
+                mlistView.clearChoices();
+            }
+        };
+
+        mAuth = FirebaseAuth.getInstance();
+
+        ItemSet temp = new ItemSet("午餐");
+        temp.addItem(new Item("銀記",1));
+        temp.addItem(new Item("咖食堂",1));
+        temp.addItem(new Item("漢堡王",1));
+        temp.addItem(new Item("海音",1));
+        temp.addItem(new Item("KFC",1));
+        temp.addItem(new Item("佐賀",1));
+        itemSets.add(temp);
 
         // 把 titles 丟到 ListView 顯示
         mlistView = findViewById(R.id.listview);
+        mlistView.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+        mlistView.setMultiChoiceModeListener(mMultiChoiceModeListener);
         refreshListView();
+
         mlistView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -74,6 +139,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
